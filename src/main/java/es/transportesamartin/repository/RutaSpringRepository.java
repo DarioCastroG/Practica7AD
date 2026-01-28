9feaabe78011104688a036c3782cb9a7c3127ea2
Dario <dariojoaquin.castro.gutierrez@iestubalcain.net>
2026-01-28 14:05:11 +0100

    Ver 0.3

--- src/main/java/es/transportesamartin/repository/RutaSpringRepository.java
+++ src/main/java/es/transportesamartin/repository/RutaSpringRepository.java
@@ -3,6 +3,20 @@ package es.transportesamartin.repository;
 import es.transportesamartin.domain.Ruta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RutaSpringRepository extends JpaRepository<Ruta, Long> {
-    //CRUD
+    //Metodos CRUD
+
+    //Crear
+    Optional<Ruta> create(Ruta ruta);
+
+    //Buscar por id
+    Optional<Ruta> findById(Long id);
+
+    //Actualizar
+    Optional<Ruta> update(Long id,Ruta ruta);
+
+    //Eliminar
+    void delete(Ruta ruta);
 }
--- src/main/java/es/transportesamartin/repository/TransportistaSpringRepository.java
+++ src/main/java/es/transportesamartin/repository/TransportistaSpringRepository.java
@@ -1,8 +1,24 @@
 package es.transportesamartin.repository;
 
 import es.transportesamartin.domain.Transportista;
+import es.transportesamartin.domain.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface TransportistaSpringRepository extends JpaRepository<Transportista, Long> {
     //CRUD
+    //Metodos CRUD
+
+    //Crear
+    Optional<Transportista> create(Transportista transportista);
+
+    //Buscar por id
+    Optional<Transportista> findById(Long id);
+
+    //Actualizar
+    Optional<Transportista> update(Long id,Transportista transportista);
+
+    //Eliminar
+    void delete(Transportista transportista);
 }

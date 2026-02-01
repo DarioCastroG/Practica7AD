9559be919eccf4a17dac4e98fd52abe22e575562
Dario <dariojoaquin.castro.gutierrez@iestubalcain.net>
2026-02-01 18:23:44 +0100

    Ver 0.5.1

--- src/main/java/es/transportesamartin/assembler/TransportistaAssembler.java
+++ src/main/java/es/transportesamartin/assembler/TransportistaAssembler.java
@@ -18,4 +18,26 @@ public class TransportistaAssembler {
 
         return dto;
     }
+
+    public Transportista toEntity(TransportistaDTO dto) {
+        if (dto == null) return null;
+
+        Transportista transportista = new Transportista();
+        mapCommonFields(dto, transportista);
+
+        return transportista;
+    }
+
+    public void updateEntity(TransportistaDTO dto, Transportista transportista) {
+        if (dto == null) return;
+
+        mapCommonFields(dto,transportista);
+    }
+    public void mapCommonFields(TransportistaDTO dto, Transportista transportista) {
+        transportista.setId(dto.getId());
+        transportista.setNombre(dto.getNombre());
+        transportista.setApellidos(dto.getApellidos());
+        transportista.setDni(dto.getDni());
+    }
+
 }
